1402ef0c2c3ddcc2f1cc5e5bfdf67482738cab20
Alexander Astrachanzev <alexejastrein@gmail.com>
2023-04-28 15:09:29 +0200

    Initial Commit

---
+++ MeTube/MeTubeApp.swift
@@ -0,0 +1,21 @@
+
+//
+//  MeTubeView.swift
+//  MeTube
+//
+//  Created by Alexander Astrachanzev on 28.04.23.
+//
+
+import SwiftUI
+
+struct MeTubeApp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeTubeApp_Previews: PreviewProvider {
+    static var previews: some View {
+        MeTubeView()
+    }
+}
---
+++ MeTube/MeTubeView.swift
@@ -0,0 +1,22 @@
+//
+//  MeTubeView.swift
+//  MeTube
+//
+//  Created by Alexander Astrachanzev on 28.04.23.
+//
+
+import SwiftUI
+
+struct MeTubeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeTubeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeTubeView()
+    }
+}
+
+
