0ee918f7a53b6fb54ca3b35e2afd76d3b1dd8d5a
Alexander Astrachanzev <alexejastrein@gmail.com>
2023-04-30 21:47:58 +0200

    build: ParticleView erstellt.

---
+++ MeTube/Views/Particle.swift
@@ -0,0 +1,27 @@
+//
+//  Particle.swift
+//  MeTube
+//
+//  Created by Alexander Astrachanzev on 30.04.23.
+//
+
+import SwiftUI
+
+// Particle Model
+
+struct Particle: Identifiable {
+    var id: UUID = .init()
+    var randomX: CGFloat = 0
+    var randomY: CGFloat = 0
+    var scale: CGFloat = 1
+    // Optional
+    var opacity: CGFloat = 1
+    
+    // Reset´s All Properties
+    mutating func reset() {
+        randomX = 0
+        randomY = 0
+        scale = 1
+        opacity = 1
+    }
+}
--- MeTube/Views/ParticleView.swift
+++ MeTube/Views/ParticleView.swift
@@ -11,5 +11,46 @@ import SwiftUI
 
 extension View {
     
+    @ViewBuilder
+    func particleEffect(systemImage: String, font: Font, status: Bool, activeTint: Color, inActiveTint: Color) -> some View {
+        self
+            .modifier(
+            ParticleModifier(systemImage: systemImage, font: font, status: status, activeTint: activeTint, inActiveTint: inActiveTint)
+            )
+    }
+}
+
+fileprivate struct ParticleModifier: ViewModifier {
+    var systemImage: String
+    var font: Font
+    var status: Bool
+    var activeTint: Color
+    var inActiveTint: Color
+    @State private var particles: [Particle] = []
     
+    func body(content: Content) -> some View {
+        content
+            .overlay(alignment: .top) {
+                ZStack{
+                    ForEach(particles) { particle in
+                        Image(systemName: systemImage)
+                            .foregroundColor(status ? activeTint : inActiveTint)
+                            .offset(x: particle.randomX, y: particle.randomY)
+                            .opacity(particle.opacity)
+                        // Only Visible if Status is inActive
+                            .opacity(status ? 1 : 0)
+                        //Base Visibility With Zero Animation
+                            .animation(.none, value: status)
+                        
+                    }
+                }
+                .onAppear {
+                if particles.isEmpty {
+                    for _ in 0...15 {
+                        let particle = Particle()
+                        }
+                    }
+                }
+        }
+    }
 }
