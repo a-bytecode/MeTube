7b8d957feb1d76077f8159779a43870dff146f15
Alexander Astrachanzev <alexejastrein@gmail.com>
2023-05-08 12:49:35 +0200

    build. MeTubeViewModel -> fetchComments Methode erstellt.

--- MeTube/Models/MeTubeViewModel.swift
+++ MeTube/Models/MeTubeViewModel.swift
@@ -10,34 +10,56 @@ import GoogleAPIClientForREST_YouTube // Import durch Cocoapods
 import YouTubePlayerKit
 
 class MeTubeViewModel : ObservableObject { // Vorlage durch: https://anthonycodesofficial.medium.com/creating-a-youtube-interface-with-swiftui-using-youtube-api-df616e099726
-//
+    //
     let secretKey: String = apiKey
-
+    
     @Published var videos: [GTLRYouTube_SearchResult] = []
     
     @Published var youTubePlayer: YouTubePlayer = YouTubePlayer()
-
-
+    
+    @Published var comments: [GTLRYouTube_CommentSnippet] = []
+    
+    
     func fetchVideos(term: String) {
-
+        
         let service = GTLRYouTubeService()
-
+        
         service.apiKey = secretKey
-
+        
         let query = GTLRYouTubeQuery_SearchList.query(withPart: ["id","snippet"])
-
+        
         query.q = term
-
+        
         query.maxResults = 5
-
+        
         service.executeQuery(query) { (ticket, response, error) in
-        if let error = error { print("Connection Error") } else {
-            self.videos = (response as! GTLRYouTube_SearchListResponse).items ?? [] }
-//            print(self.videos[0])
+            if let error = error { print("Connection Error") } else {
+                self.videos = (response as! GTLRYouTube_SearchListResponse).items ?? [] }
+            //            print(self.videos[0])
         }
-
+        
     }
     
+    func fetchComments(videoId: String, completion: @escaping ([GTLRYouTube_CommentSnippet]?) -> Void) {
+        let service = GTLRYouTubeService()
+        service.apiKey = secretKey
+        let query = GTLRYouTubeQuery_CommentThreadsList.query(withPart: ["id","snippet"])
+        query.videoId = videoId
+        query.maxResults = 5
+        service.executeQuery(query) { (ticket, response, error) in
+            if let error = error {
+                print("Connection Error")
+                completion(nil)
+            } else {
+                let commentThreads = (response as! GTLRYouTube_CommentThreadListResponse).items ?? []
+                var comments: [GTLRYouTube_CommentSnippet] = []
+                for commentThread in commentThreads {
+                    if let comment = commentThread.snippet?.topLevelComment?.snippet {
+                        comments.append(comment)
+                    }
+                }
+                completion(comments)
+            }
+        }
+    }
 }
-
-
