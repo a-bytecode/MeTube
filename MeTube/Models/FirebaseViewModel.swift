70d1bae38e2bcc1fdc7d46699c7f06ff73c7fc2c
Alexander Astrachanzev <alexejastrein@gmail.com>
2023-05-12 10:48:22 +0200

    build: Firebase Einstiegspunkt @main implementiert.

---
+++ MeTube/Models/FirebaseViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FirebaseViewModel.swift
+//  MeTube
+//
+//  Created by Alexander Astrachanzev on 12.05.23.
+//
+
+import Foundation
+
+
--- MeTube/Views/MainView/MeTubeApp.swift
+++ MeTube/Views/MainView/MeTubeApp.swift
@@ -1,8 +1,19 @@
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+      
+    return true
+  }
+}
 
 @main
 struct MeTubeApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     @StateObject var viewModel : MeTubeViewModel = MeTubeViewModel()
     @State private var searchTerm = ""
