d09045bd01c4f0c67e1417687dd0462b77e58361
Alexander Astrachanzev <alexejastrein@gmail.com>
2023-05-12 11:07:27 +0200

    build: FirebaseViewModel, Sign in & Sign Out Funktion erstellt.

--- MeTube/Models/FirebaseViewModel.swift
+++ MeTube/Models/FirebaseViewModel.swift
@@ -6,5 +6,44 @@
 //
 
 import Foundation
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+
+
+
+
+class FirebaseViewModel: ObservableObject {
+    
+    let db = Firestore.firestore()
+    
+    @Published var email = ""
+    @Published var password = ""
+    @Published var isLoggedIn = false
+    
+    func saveToFireStore() {
+        print("Speichert...")
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+            guard let self = self else { return }
+            if let error = error {
+                print("Login fehlgeschlagen: \(error.localizedDescription)")
+                
+            } else {
+                print("Login in as: \(authResult?.user.email ?? "")")
+                self.isLoggedIn = true
+            }
+        }
+    }
+    
+    func logout() {
+        
+        do {
+            try Auth.auth().signOut()
+            self.isLoggedIn = false
+        } catch let signOutError as NSError {
+            print("Fehler beim ausloggen: %@", signOutError)
+        }
+    }
+}
 
 
