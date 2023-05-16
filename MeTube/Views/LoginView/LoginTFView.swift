aafe1ddd47981c1f73fe168cede6c7316d1ceec5
Alexander Astrachanzev <alexejastrein@gmail.com>
2023-05-16 13:34:14 +0200

    build: Tint im TextField versucht Custom umzuarbeiten.

--- MeTube/Views/LoginView/LoginTFView.swift
+++ MeTube/Views/LoginView/LoginTFView.swift
@@ -14,21 +14,34 @@ struct LoginTFView: View {
     var body: some View {
         
         VStack {
-            
-
-            ZStack {
-                Rectangle()
-                    .fill(Color.black)
-                    .frame(height: 50)
-                    .overlay(Rectangle().stroke(Color.white, lineWidth: 2))
-
+            HStack {
+                ZStack {
+                    
+                    Rectangle()
+                        .fill(Color.black)
+                        .frame(height: 50)
+                        .overlay(Rectangle().stroke(Color.white, lineWidth: 2))
+                    
+                    if input == "" {
+                        Text("Email")
+                            .foregroundColor(Color.white)
+                            .offset(x: -120)
+                        TextField("Email", text: $input)
+                            .frame(width: .infinity, height: 40)
+                            .foregroundColor(Color.white)
+                            .tint(Color.red)
+                            .accentColor(Color.red)
+                            .padding(20)
+                } else {
                     TextField("Email", text: $input)
                         .frame(width: .infinity, height: 40)
                         .foregroundColor(Color.white)
                         .tint(Color.red)
                         .accentColor(Color.red)
                         .padding(20)
+                }
             }
+        }
 
         }.padding(.horizontal, 30)
     }
