8bcd89f69ec904c72b4459eefc1a7b04e70e13dd
Alexander Astrachanzev <alexejastrein@gmail.com>
2023-05-26 17:05:55 +0200

    build: Firebase "Clean up", neue MainView erstellt.

---
+++ MeTube/Views/MainView/MainView.swift
@@ -0,0 +1,40 @@
+//
+//  MainView.swift
+//  MeTube
+//
+//  Created by Alexander Astrachanzev on 26.05.23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    
+    @State var fbViewModel: FirebaseViewModel = FirebaseViewModel()
+    @State var viewModel: MeTubeViewModel = MeTubeViewModel()
+    @State private var searchTerm = ""
+    @State private var input = ""
+    @State private var isLoggedIn = false
+    
+    var body: some View {
+        
+        NavigationStack {
+            LoginView()
+                .onAppear {
+                    fbViewModel.checkUser()
+                }
+            NavigationLink(destination: SearchView(input: input, searchTerm: searchTerm)
+                           
+                           ,isActive: $isLoggedIn
+            ) {
+                EmptyView()
+            }
+        }.environmentObject(viewModel)
+         .environmentObject(fbViewModel)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
--- MeTube/Views/MainView/MeTubeApp.swift
+++ MeTube/Views/MainView/MeTubeApp.swift
@@ -6,7 +6,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     FirebaseApp.configure()
-      
+
     return true
   }
 }
@@ -15,30 +15,12 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 struct MeTubeApp: App {
     //Registration für Firebase Setup!
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
-    
-    @StateObject var viewModel : MeTubeViewModel = MeTubeViewModel()
-    @State var fbViewModel = FirebaseViewModel()
-    @State private var searchTerm = ""
-    @State private var input = ""
 
     var body: some Scene { // TODO: EnviromentObject als übergeordneten Datentyp erstellen
         
         WindowGroup {
 
-            NavigationStack {
-                LoginView()
-                    .onAppear {
-                        fbViewModel.checkUser()
-                    }
-                NavigationLink(destination: SearchView(input: input, searchTerm: searchTerm)
-                   
-                               ,isActive: $fbViewModel.isLoggedIn
-                ) {
-                    EmptyView()
-                }
-            }.environmentObject(viewModel)
-             .environmentObject(fbViewModel)
+            MainView()
         }
     }
 }
