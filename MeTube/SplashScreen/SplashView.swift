4b2ee79fd7c1e86ff566294aa68d651c9441677d
Alexander Astrachanzev <alexejastrein@gmail.com>
2023-06-09 14:34:59 +0200

    layout: Splashscreenbackground angelegt. (via settingsViewModel)

--- MeTube/SplashScreen/SplashView.swift
+++ MeTube/SplashScreen/SplashView.swift
@@ -18,39 +18,58 @@ struct SplashView: View {
     @State private var duration: Double = 4.0
     @State private var pulseAmount = 1.0
     @State private var autoreverses = false
+    @StateObject var settingsViewModel: SettingsViewModel = SettingsViewModel()
+    private var url = URL(string: "https://media.giphy.com/media/26hitlJ1tvqhlUWnm/giphy.gif")!
+
 
     var body: some View {
         NavigationStack {
-            VStack {
+            ZStack {
                 
-                if isAnimating {
-                    Image("Logo2")
-                        .padding()
-                        .foregroundColor(Color.white)
-                        .font(.title3)
-                        .multilineTextAlignment(.center)
-                        .bold()
-                        .background(LinearGradient(colors: [.green,.yellow], startPoint: .topLeading, endPoint: .bottomTrailing))
-                        .clipShape(Rectangle())
-                        .overlay(Rectangle().stroke(.black, lineWidth: 3))
-                        .shadow(radius: 5,x: 2,y: 3)
-                        .rotationEffect(.degrees(isRotating))
-                        .animation(.easeInOut(duration: duration), value: isRotating)
-                        .onAppear {
-                            isRotating += 360
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
-                                isAnimating = false
-                                isDone = true
+                if let image = settingsViewModel.animatedImage {
+                    FLAnimatedImageViewWrapper(image: image)
+                        .frame(width: 100.0, height: .infinity)
+                    
+                } else {
+                    Text("Loading...")
+                }
+                VStack {
+                    
+                    
+                    
+                    
+                    if isAnimating {
+                        Image("Logo2")
+                            .padding()
+                            .foregroundColor(Color.white)
+                            .font(.title3)
+                            .multilineTextAlignment(.center)
+                            .bold()
+                            .background(LinearGradient(colors: [.green,.yellow], startPoint: .topLeading, endPoint: .bottomTrailing))
+                            .clipShape(Rectangle())
+                            .overlay(Rectangle().stroke(.black, lineWidth: 3))
+                            .shadow(radius: 5,x: 2,y: 3)
+                            .rotationEffect(.degrees(isRotating))
+                            .animation(.easeInOut(duration: duration), value: isRotating)
+                            .onAppear {
+                                isRotating += 360
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+                                    isAnimating = false
+                                    isDone = true
+                                }
                             }
-                        }
+                    }
+                    NavigationLink(destination: MainView(), isActive: $isDone) {
+                        EmptyView()
+                    }
                 }
-                NavigationLink(destination: MainView(), isActive: $isDone) {
-                    EmptyView()
-                }
-            }
+            }.edgesIgnoringSafeArea(.all)
         }.onAppear {
             isAnimating = true
+            settingsViewModel.loadAnimatedImage(from: url)
         }
+        
+
     }
 }
 
